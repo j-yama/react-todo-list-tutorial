a290398ee75b15126f2cac9414aa62550ccca5e2
j-yama <j-yama@example.com>
2018-09-17 23:31:51 +0900

    todoが表示されるようにした

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TodoList from './TodoList'
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: "Hello, React!",
+          desc: "React始めました",
+          done: false,
+        },
+        {
+          id: 2,
+          title: "Hellow, Redux!",
+          desc: "Reduxも始めました",
+          done: false,
+        },
+      ]
+    };
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <h1>todoアプリを作ってみた</h1>
+        <TodoList
+          todos={this.state.todos}
+        />
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
